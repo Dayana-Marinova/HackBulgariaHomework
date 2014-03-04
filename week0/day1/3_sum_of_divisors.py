4650ff2c5989e81a225737894615f087a442d16f
didi <didi@didi-HP-Pavilion-g6-Notebook-PC.(none)>
2014-03-04 16:31:45 +0200

    Add main function to sum of divisors

--- week0/day1/3_sum_of_divisors.py
+++ week0/day1/3_sum_of_divisors.py
@@ -5,7 +5,12 @@ def sum_of_divisors(n):
 			sum += i
 	return sum 
 
-print sum_of_divisors(8)
-print sum_of_divisors(7)
-print sum_of_divisors(1)
-print sum_of_divisors(1000)
+def main():
+
+	print sum_of_divisors(8)
+	print sum_of_divisors(7)
+	print sum_of_divisors(1)
+	print sum_of_divisors(1000)
+
+if __name__ == '__main__':
+	main()
