5334935630b94dc9d0c7406f6683fd71c7c9d960
didi <didi@didi-HP-Pavilion-g6-Notebook-PC.(none)>
2014-03-04 16:41:19 +0200

    Add main function to sevens in a row

--- week0/day1/6_sevens_in_a_row.py
+++ week0/day1/6_sevens_in_a_row.py
@@ -8,8 +8,12 @@ def sevens_in_a_row(arr, n):
 		else:
 			num = 0
 	return False
-		
-print sevens_in_a_row([10,8,7,6,7,7,7,20,-7], 3)
-print sevens_in_a_row([1,7,1,7,7], 4)
-print sevens_in_a_row([7,7,7,1,1,1,7,7,7,7], 3)
-print sevens_in_a_row([7,2,1,6,2], 1)
+
+def main():		
+	print sevens_in_a_row([10,8,7,6,7,7,7,20,-7], 3)
+	print sevens_in_a_row([1,7,1,7,7], 4)
+	print sevens_in_a_row([7,7,7,1,1,1,7,7,7,7], 3)
+	print sevens_in_a_row([7,2,1,6,2], 1)
+
+if __name__ == '__main__':
+	main()
