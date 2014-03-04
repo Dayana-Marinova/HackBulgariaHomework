d5616f9101bc7353e31e62511bfa17a737a79bec
didi <didi@didi-HP-Pavilion-g6-Notebook-PC.(none)>
2014-03-04 16:26:44 +0200

    Add main function to sum_of_digits

--- week0/day1/1_sum_of_digits.py
+++ week0/day1/1_sum_of_digits.py
@@ -8,8 +8,15 @@ def sum_of_digits(n):
 		n = n // 10
 	return m
 
-print sum_of_digits(-123)
-print sum_of_digits(1325132435356)
-print sum_of_digits(6)
-print sum_of_digits(123)
-print sum_of_digits(-10)
+
+def main():
+
+	print sum_of_digits(-123)
+	print sum_of_digits(1325132435356)
+	print sum_of_digits(6)
+	print sum_of_digits(123)
+	print sum_of_digits(-10)
+
+
+if __name__ == '__main__':
+	main()
