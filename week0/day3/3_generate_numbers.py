28d8ccb97d5790f99d8dbe450fabdbbc974242bc
didi <didi@didi-HP-Pavilion-g6-Notebook-PC.(none)>
2014-03-05 03:08:17 +0200

    Eliminating unnecessary rows

--- week0/day3/3_generate_numbers.py
+++ week0/day3/3_generate_numbers.py
@@ -11,11 +11,7 @@ def generate_numbers(filename,n):
 	file.close()
 
 def main():
-	# Didi, krusti si argv promenlivata po drug nachin
-	# taka shte se oburkash
 	generate_numbers(sys.argv[1], sys.argv[2])
-
-    #generate_numbers("filename.txt", 10)
 
 if __name__ == '__main__':
     main()
