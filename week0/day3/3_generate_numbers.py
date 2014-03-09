a59115168816eadf4514b91924c66d4bae2e5107
didi <dayana3264@gmail.com>
2014-03-09 06:41:23 +0200

    something

--- week0/day3/3_generate_numbers.py
+++ week0/day3/3_generate_numbers.py
@@ -2,16 +2,16 @@ import sys
 from random import randint
 
 def generate_numbers(filename,n):
-	file = open(filename, "w")
-	contents= []
-	for i in range(0,int(n)):
-		contents.append(str(randint(0,1000)))
-	
-	file.write(", ".join(contents))
-	file.close()
+    file = open(filename, "w")
+    contents= []
+    for i in range(0,int(n)):
+        contents.append(str(randint(0,1000)))
+    
+    file.write(", ".join(contents))
+    file.close()
 
 def main():
-	generate_numbers(sys.argv[1], sys.argv[2])
+    generate_numbers(sys.argv[1], sys.argv[2])
 
 if __name__ == '__main__':
     main()
