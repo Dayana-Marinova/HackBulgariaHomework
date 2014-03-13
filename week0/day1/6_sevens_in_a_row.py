9f85255e8370af1682568d2b2892f2f0b7b98845
didi <dayana3264@gmail.com>
2014-03-13 09:51:45 +0200

    Modified the function sevens in a row

--- week0/day1/6_sevens_in_a_row.py
+++ week0/day1/6_sevens_in_a_row.py
@@ -1,19 +1,23 @@
 def sevens_in_a_row(arr, n):
-	num = 0
-	for i in range(0, len(arr)):
-		if arr[i] == 7:
-			num += 1
-			if num == n:
-				return True
-		else:
-			num = 0
-	return False
+    num = 0
+    if not 7 in arr and n == 0:
+        return True
+    for i in range(0, len(arr)):
+        if arr[i] == 7:
+            num += 1
+            if num == n:
+                return True
+        else:
+            num = 0
+    return False
 
-def main():		
-	print sevens_in_a_row([10,8,7,6,7,7,7,20,-7], 3)
-	print sevens_in_a_row([1,7,1,7,7], 4)
-	print sevens_in_a_row([7,7,7,1,1,1,7,7,7,7], 3)
-	print sevens_in_a_row([7,2,1,6,2], 1)
+
+def main():
+    print sevens_in_a_row([10, 8, 7, 6, 7, 7, 7, 20, -7], 3)
+    print sevens_in_a_row([1, 7, 1, 7, 7], 4)
+    print sevens_in_a_row([7, 7, 7, 1, 1, 1, 7, 7, 7, 7], 3)
+    print sevens_in_a_row([7, 2, 1, 6, 2], 1)
+    print sevens_in_a_row([10, 2, 1, 6, 2], 0)
 
 if __name__ == '__main__':
-	main()
+    main()
