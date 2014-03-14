a9f9a96a03a01541cd9bbe9f54a8feed96291c1e
didi <dayana3264@gmail.com>
2014-03-14 23:17:06 +0200

    Add samo if`s to the function

--- week0/day2/29_simplify_fraction.py
+++ week0/day2/29_simplify_fraction.py
@@ -1,6 +1,10 @@
 def simplify_fraction(fraction):
     nominatior = fraction[0]
     denominator = fraction[1]
+    if nominatior == 0:
+        return "we can not divide by zero"
+    if denominator == 0:
+        return (0, 0)
     max_number = max(nominatior, denominator)
     for i in range(2, max_number):
         if nominatior % i == 0 and denominator % i == 0:
