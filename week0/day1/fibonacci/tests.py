3446c4159325fea3e0abe15bf2e3424317aeb29f
didi <dayana3264@gmail.com>
2014-03-21 01:12:15 +0200

    Add fibonacci folder

---
+++ week0/day1/fibonacci/tests.py
@@ -0,0 +1,22 @@
+import solution
+import unittest
+
+
+class FibonacciTest(unittest.TestCase):
+    def test_fibonacci_1(self):
+        self.assertEqual(1, solution.nth_fibonacci(1))
+
+    def test_fibonacci_2(self):
+        self.assertEqual(1, solution.nth_fibonacci(2))
+
+    def test_fibonacci_8(self):
+        self.assertEqual(21, solution.nth_fibonacci(8))
+
+    def test_fibonacci_14(self):
+        self.assertEqual(377, solution.nth_fibonacci(14))
+
+    def test_fibonacci_100(self):
+        self.assertEqual(354224848179261915075, solution.nth_fibonacci(100))
+
+if __name__ == '__main__':
+    unittest.main()
