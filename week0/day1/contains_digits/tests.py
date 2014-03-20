13850d32dbc50dae0c461a51aa64c17c6cab6a76
didi <dayana3264@gmail.com>
2014-03-21 01:08:01 +0200

    Add contains_digits folder

---
+++ week0/day1/contains_digits/tests.py
@@ -0,0 +1,23 @@
+import solution
+import unittest
+
+
+class ContainsDigitsTest(unittest.TestCase):
+    def test_contains_digits1(self):
+        self.assertEqual(True, solution.contains_digits(10001, [0, 1]))
+
+    def test_contains_digits2(self):
+        self.assertEqual(True, solution.contains_digits(123332, [2, 1]))
+
+    def test_contains_digits3(self):
+        self.assertEqual(False, solution.contains_digits(0000, [1, 5]))
+
+    def test_contains_digits4(self):
+        self.assertEqual(False, solution.contains_digits(3464, [4, 5]))
+
+    def test_contains_digits5(self):
+        self.assertEqual(False, solution.contains_digits(42, [1, 5]))
+
+
+if __name__ == '__main__':
+    unittest.main()
