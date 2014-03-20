b6f7e13d4db0d3de6fa5711bc17f52a0fde6843d
didi <dayana3264@gmail.com>
2014-03-21 01:16:56 +0200

    Add is_number_balanced folder with tests

---
+++ week0/day1/is_number_balanced/tests.py
@@ -0,0 +1,22 @@
+import solution
+import unittest
+
+
+class IsBalanceTest(unittest.TestCase):
+    def test_is_number_balanced1(self):
+        self.assertEqual(True, solution.is_number_balanced(10001))
+
+    def test_is_number_balanced2(self):
+        self.assertEqual(True, solution.is_number_balanced(14332))
+
+    def test_is_number_balanced3(self):
+        self.assertEqual(False, solution.is_number_balanced(4859))
+
+    def test_is_number_balanced4(self):
+        self.assertEqual(False, solution.is_number_balanced(1231))
+
+    def test_is_number_balanced5(self):
+        self.assertEqual(True, solution.is_number_balanced(1238033))
+
+if __name__ == '__main__':
+    unittest.main()
