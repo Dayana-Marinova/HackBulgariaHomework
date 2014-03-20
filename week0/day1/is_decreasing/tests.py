cec9c08b5fbe884dcf69c7640c9d11c2d35460c7
didi <dayana3264@gmail.com>
2014-03-21 01:13:03 +0200

    Add is_decreasing folder

---
+++ week0/day1/is_decreasing/tests.py
@@ -0,0 +1,22 @@
+import solution
+import unittest
+
+
+class IsDecreasingTest(unittest.TestCase):
+    def test_is_decreasing1(self):
+        self.assertEqual(True, solution.is_decreasing([5, 4, 3, 2, 1]))
+
+    def test_is_decreasing2(self):
+        self.assertEqual(True, solution.is_decreasing([100, 50, 20]))
+
+    def test_is_decreasing3(self):
+        self.assertEqual(False, solution.is_decreasing([10, 5, 10]))
+
+    def test_is_decreasing4(self):
+        self.assertEqual(False, solution.is_decreasing([1, 2, 3, 4]))
+
+    def test_is_decreasing5(self):
+        self.assertEqual(True, solution.is_decreasing([2, 2, 2, 2]))
+
+if __name__ == '__main__':
+    unittest.main()
