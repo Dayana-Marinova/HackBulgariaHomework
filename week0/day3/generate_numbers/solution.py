afb8428886e46c852209991b259699e41dbaa21f
didi <dayana3264@gmail.com>
2014-03-21 01:58:47 +0200

    Add all files to folders

---
+++ week0/day3/generate_numbers/solution.py
@@ -0,0 +1,19 @@
+import sys
+from random import randint
+
+
+def generate_numbers(filename, n):
+    file = open(filename, "w")
+    contents = []
+    for i in range(0, int(n)):
+        contents.append(str(randint(0, 1000)))
+
+    file.write(", ".join(contents))
+    file.close()
+
+
+def main():
+    generate_numbers(sys.argv[1], sys.argv[2])
+
+if __name__ == '__main__':
+    main()
