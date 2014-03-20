7da1f868026cf5e12144f5f84059c8dc24d986b4
didi <dayana3264@gmail.com>
2014-03-21 01:22:28 +0200

    Add factorization folder

---
+++ week0/day1/prime_factorization/tests.py
@@ -0,0 +1,22 @@
+import solution
+import unittest
+
+
+class FantorizationTest(unittest.TestCase):
+    def test_prime_factorization1(self):
+        self.assertEqual({2: 2, 5: 1}, solution.prime_factorization(20))
+
+    def test_prime_factorization2(self):
+        self.assertEqual({2: 2, 89: 1}, solution.prime_factorization(356))
+
+    def test_prime_factorization3(self):
+        self.assertEqual({2: 1, 5: 1}, solution.prime_factorization(10))
+
+    def test_prime_factorization4(self):
+        self.assertEqual({89: 1}, solution.prime_factorization(89))
+
+    def test_prime_factorization5(self):
+        self.assertEqual({2: 3, 5: 3}, solution.prime_factorization(1000))
+
+if __name__ == '__main__':
+    unittest.main()
