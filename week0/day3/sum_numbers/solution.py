59991c487d0043d13efa8f1ae907836b31648d1b
didi <dayana3264@gmail.com>
2014-03-24 02:51:59 +0200

    Repair the function

--- week0/day3/sum_numbers/solution.py
+++ week0/day3/sum_numbers/solution.py
@@ -1,21 +1,19 @@
 import sys
-from random import randint
 
 
-def sum_numbers(filename, n):
+def sum_numbers(filename):
     sum = 0
-    file = open(filename, "w")
-    contents = []
-    for i in range(0, int(n)):
-        contents.append(str(randint(0, 1000)))
-        sum += int(contents[i])
-    file.write(" ".join(contents))
-    file.close()
-    print sum
+    file = open(filename, "r")
+    content = file.read()
+    countent_numbers = content.split(" ")
+    for number in countent_numbers:
+        number = int(number)
+        sum += number
+    return sum
 
 
 def main():
-    sum_numbers(sys.argv[1], sys.argv[2])
+    sum_numbers(sys.argv[1])
 
 if __name__ == '__main__':
     main()
