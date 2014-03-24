10cc098d791fcd23a1967019e0c180cd1e00880f
didi <dayana3264@gmail.com>
2014-03-24 02:48:03 +0200

    Repair cat function

--- week0/day3/cat/solution.py
+++ week0/day3/cat/solution.py
@@ -2,14 +2,13 @@ import sys
 
 
 def cat(filename):
-    if (sys.argv) > 1:
-        file = open(filename, 'r')
-        return file.read()
-    else:
-        return 'no argv given'
+    file = open(filename, 'r')
+    return file.read()
 
 
 def main():
+    if len(sys.argv) <= 1:
+        return 'no argv given'
     for argv in range(1, len(sys.argv)):
         cat(sys.argv[argv])
 
